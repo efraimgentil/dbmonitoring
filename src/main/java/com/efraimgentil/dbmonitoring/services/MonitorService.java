8c1023690dc1e1c5f35f178c55150ffdfde782b3
EfraimGentil <efraim.gentil@gmail.com>
2013-12-08 22:09:35 -0300

    #8 Addition of the service MonitorService, refactoring of the MonitorServlet

---
+++ src/main/java/com/efraimgentil/dbmonitoring/services/MonitorService.java
@@ -0,0 +1,56 @@
+package com.efraimgentil.dbmonitoring.services;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.efraimgentil.dbmonitoring.connections.ConnectionPool;
+import com.efraimgentil.dbmonitoring.models.MonitorInfo;
+import com.efraimgentil.dbmonitoring.models.MonitorResponse;
+
+/**
+ * 
+ * @author Efraim Gentil
+ * @date Dec 8, 2013
+ */
+public class MonitorService {
+	
+	public MonitorResponse openConnection(MonitorInfo monitorInfo){
+		try {
+			ConnectionPool.getInstance().openConnection(monitorInfo);
+			Map<String, Object> data = new HashMap<>();
+			data.put("token", monitorInfo.getHost());
+			return new MonitorResponse(true, "Connection was open with success", data);
+		} catch (ClassNotFoundException e) {
+			return new MonitorResponse(false, "Was not possible to find the connection driver", null );
+		} catch (SQLException e) {
+			return new MonitorResponse(false,
+							"Was not possible to open the connection, verify the connection information. Error: "
+							+ e.getMessage(), null );
+		}
+	}
+	
+	public MonitorResponse processQuery( String token , String query ){
+		try {
+			Connection conn = ConnectionPool.getInstance().getConnection( token );
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			SimpleDateFormat sdf = new SimpleDateFormat(
+					"dd/MM/yyyy as HH:mm:ss");
+			MonitorResponse monitorResponse = new MonitorResponse( true , null , new HashMap<String,Object>() );
+			monitorResponse.getData().put("updateDate", sdf.format(new Date()) );
+			monitorResponse.createRows(rs);
+			return monitorResponse;
+		} catch (SQLException e) {
+			return new MonitorResponse( false , "Was not possible to execute the query. Error: " + e.getMessage() , null ); 
+		} catch (Exception e) {
+			return new MonitorResponse( false , "Unexpected error. Error: " + e.getMessage() , null );
+		}
+	}
+	
+}
--- src/main/java/com/efraimgentil/dbmonitoring/servlets/MonitorServlet.java
+++ src/main/java/com/efraimgentil/dbmonitoring/servlets/MonitorServlet.java
@@ -31,12 +31,13 @@ import com.efraimgentil.dbmonitoring.connections.ConnectionPool;
 import com.efraimgentil.dbmonitoring.constants.AvailableDatabase;
 import com.efraimgentil.dbmonitoring.models.MonitorInfo;
 import com.efraimgentil.dbmonitoring.models.MonitorResponse;
+import com.efraimgentil.dbmonitoring.services.MonitorService;
 
 @WebServlet({ "/monitor" })
 public class MonitorServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private static final String INITIATE = "INITIATE";
 	private static final String UPDATE = "UPDATE";
 
@@ -49,57 +50,44 @@ public class MonitorServlet extends HttpServlet {
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("application/json");
 		response.setCharacterEncoding("UTF-8");
-		
-		Map<String, Object> data = new HashMap<>();
-		
-		String action = request.getParameter("action") != null ? request.getParameter("action") : null;
-		
+
+		MonitorService monitorService = new MonitorService();
+
+		String action = request.getParameter("action") != null ? request
+				.getParameter("action") : null;
+
 		OutputStream outputStream = response.getOutputStream();
-		
+
 		ObjectMapper mapper = new ObjectMapper();
-     	if ( INITIATE.equalsIgnoreCase(action) ) {
-     		ObjectReader objectReader =  mapper.reader(MonitorInfo.class);
-         	MonitorInfo monitorInfo = objectReader.readValue( request.getParameter("form") );
-			try {
-				ConnectionPool.getInstance().openConnection( monitorInfo );
-				data.put("token", monitorInfo.getHost() );
-				mapper.writeValue( outputStream ,  new MonitorResponse( true , "Connection was open with success" , data ) );
-			} catch (ClassNotFoundException e) {
-				mapper.writeValue( outputStream ,  new MonitorResponse( false , "Was not possible to find the connection driver" , null ) );
-			} catch (SQLException e) {
-				mapper.writeValue( outputStream ,  new MonitorResponse( false , "Was not possible to open the connection, verify the connection information. Error: " + e.getMessage() , null ) );
-			}
-			return;
+		
+		switch (action) {
+			case INITIATE:
+				ObjectReader objectReader = mapper.reader(MonitorInfo.class);
+				MonitorInfo monitorInfo = objectReader.readValue(request.getParameter("form"));
+				MonitorResponse monitorResponse = monitorService.openConnection(monitorInfo);
+				mapper.writeValue(outputStream, monitorResponse);
+				break;
+			case UPDATE:
+				Object monitorTitleO = request.getParameter("monitorTitle");
+				Object tokenO = request.getParameter("token");
+				Object queryO = request.getParameter("query");
+				String token = tokenO != null ? (String) tokenO : null;
+				String monitorTitle = monitorTitleO != null ? (String) monitorTitleO
+						: null;
+				String query = queryO != null ? (String) queryO : null;
+				if (query != null && (token != null && !token.isEmpty())) {
+					mapper.writeValue(outputStream,
+							monitorService.processQuery(token, query));
+				} else {
+					mapper.writeValue(outputStream, new MonitorResponse(false,
+							"There is no query to be executed", null));
+				}
+				break;
+			default:
+				mapper.writeValue(outputStream, new MonitorResponse(false, "This is not a valid action" , null));
+				break;
 		}
 
-     	if (UPDATE.equalsIgnoreCase(action)) {
-			Object monitorTitleO = request.getParameter("monitorTitle");
-			Object tokenO = request.getParameter("token");
-			Object queryO = request.getParameter("query");
-			String token = tokenO != null ? (String) tokenO : null;
-			String monitorTitle = monitorTitleO != null ? (String) monitorTitleO : null;
-			String query = queryO != null ? (String) queryO : null;
-			if ( query != null && ( token != null && !token.isEmpty() ) )
-				try {
-					Connection conn = ConnectionPool.getInstance().getConnection( token );
-					Statement stmt = conn.createStatement();
-					ResultSet rs = stmt.executeQuery(query);
-					SimpleDateFormat sdf = new SimpleDateFormat(
-							"dd/MM/yyyy as HH:mm:ss");
-					MonitorResponse monitorResponse = new MonitorResponse( true , null , new HashMap<String,Object>() );
-					monitorResponse.getData().put("updateDate", sdf.format(new Date()) );
-					monitorResponse.createRows(rs);
-					mapper.writeValue( outputStream , monitorResponse );
-				} catch (SQLException e) {
-					mapper.writeValue( outputStream ,  new MonitorResponse( false , "Was not possible to execute the query. Error: " + e.getMessage() , null ) ); 
-					e.printStackTrace();
-				}
-			else
-				mapper.writeValue( outputStream ,  new MonitorResponse( false , "There is no query to be executed" , null ) );
-			
-			return;
-		}
-		mapper.writeValue( outputStream ,  new MonitorResponse( false , "This is not a valid action" , null ) );
 	}
 
 }
