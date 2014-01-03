8f0e1ff03ceca852c3dd172e4efd85221d5add82
EfraimGentil <efraim.gentil@gmail.com>
2014-01-03 00:26:53 -0300

    #3 Refatoring of the dbmonitoring.js , separation of responsibilities

--- src/main/webapp/resources/js/dbmonitoring.js
+++ src/main/webapp/resources/js/dbmonitoring.js
@@ -4,6 +4,8 @@ var Monitor = function(){
 	
 	var scope = this;
 	
+	var resultArea = new ResultArea($("#result")[0]);
+	
 	this.createConnection = function( eve ){
 		var btn = eve.target;
 		if (this.validateNewMonitorForm() && !btn.classList.contains( "disabled" ) ) {
@@ -58,7 +60,7 @@ var Monitor = function(){
 
 	this.successfullyCreateQuery = function( data , status , jqXHR ){
 		if (data.success) {
-			this.updateResultArea(data);
+			resultArea.updateResultArea(data);
 			var refreshTime = $("#form-monitor-query").find("#refresh-time").val() || 5;
 			var monitorTitle = $("#form-monitor-query").find("#monitor-title").val() || "No title defined";
 			$("#title-monitoring").html(monitorTitle);
@@ -133,55 +135,24 @@ var Monitor = function(){
 				)
 			},
 			success :  function(data , status, jqXHR){ 
-				scope.updateResultArea(data);
+				if(data.success){
+					resultArea.updateResultArea( data );
+				}else{
+					scope.stopInterval();
+					resultArea.addErrorMessage( data.message );
+				}
 			},
 			error :  function(jqXHR, status, errorThrown) {
-				this.manageException ( jqXHR , status , errorThrown );
-				this.stopInterval();
+//				scope.manageException ( jqXHR , status , errorThrown );
+				resultArea.addErrorMessage( "Well that was unexpected " + errorThrown );
+				scope.stopInterval();
 			}
 		});	
 	};
 	
-	this.updateResultArea = function(json){
-		$("#result").empty();
-		if (json != "") {
-			if (json.success && json.data) {
-				var rows = json.data.rows;
-				$("#dataAtualizacao").text(json.dataAtualizacao);
-				if (rows && rows.length > 0) {
-					var j = rows[0];
-					var arr = [];
-					arr.push('<table class="table table-striped"><tr>');
-					for (k in j) {
-						arr.push("<th>");
-						arr.push("" + k);
-						arr.push("</th>");
-					}
-					arr.push("</tr>");
-					var rowsLength = rows.length;
-					for (var i = 0; i < rowsLength ; i++) {
-						var row = rows[i];
-						arr.push("<tr>");
-						for (k in row) {
-							arr.push("<td>");
-							arr.push(row[k]);
-							arr.push("</td>");
-						}
-						arr.push("</tr>");
-					}
-					arr.push('</table>');
-					$("#result").append( arr.join("") );
-				}
-			} else {
-				$("#result").append("<span style='color:red;'>" + json.message + "</span>");
-				this.stopInterval();
-			}
-		}
-	};
-	
 	this.stopInterval = function(){
 		if (interval != null) {
-			this.clearInterval(interval);
+			window.clearInterval(interval);
 			interval = null;
 		}
 	};
@@ -224,7 +195,67 @@ var Monitor = function(){
 	
 };
 
+var ResultArea = function(resultArea){
+	
+	this.resultArea = resultArea;
+	
+	this.addErrorMessage = function(message){
+		$(this.resultArea).empty();
+		$(this.resultArea).append("<span style='color:red;'>" + message + "</span>");
+	};
+	
+	this.updateResultArea = function(json){
+		$(this.resultArea).empty();
+		var rows = json.data.rows;
+		$("#dataAtualizacao").text(json.dataAtualizacao);
+		if (rows && rows.length > 0) {
+			var j = rows[0];
+			var arr = [];
+			arr.push('<table class="table table-striped"><tr>');
+			for (k in j) {
+				arr.push("<th>");
+				arr.push("" + k);
+				arr.push("</th>");
+			}
+			arr.push("</tr>");
+			var rowsLength = rows.length;
+			for (var i = 0; i < rowsLength ; i++) {
+				var row = rows[i];
+				arr.push("<tr>");
+				for (k in row) {
+					arr.push("<td>");
+					arr.push(row[k]);
+					arr.push("</td>");
+				}
+				arr.push("</tr>");
+			}
+			arr.push('</table>');
+			$(this.resultArea).append( arr.join("") );
+		}else{
+			this.addErrorMessage("Empty result");
+		}
+	};
+	
+	this.resizeTheResultArea = function() {
+		var titleHeight = $("#row-title").height();
+		var marginBottom = 10;
+		var availableHeight = $(document).height();
+		$("#row-query").height(availableHeight - (titleHeight + marginBottom));
+	};
+	
+};
+
 $(document).ready(function() {
+	
+	/**
+	 * Verify the sizeof the screen and update the result area to match it
+	 */
+	function resizeTheResultArea() {
+		var titleHeight = $("#row-title").height();
+		var marginBottom = 10;
+		var availableHeight = $(document).height();
+		$("#row-query").height(availableHeight - (titleHeight + marginBottom));
+	}
 	
 	$(window).resize( resizeTheResultArea );
 	resizeTheResultArea();
@@ -243,220 +274,4 @@ $(document).ready(function() {
 		monitor.stopMonitor(e);
 	});
 	
-//	$("#btnInitiateMonitor").click(function(){
-//		var btn = this;
-//		if (validateMonitorQueryForm() && !btn.classList.contains( "disabled" ) ) {
-//			btn.classList.add( "disabled" );
-//			$.ajax({
-//				url  : $("#form-monitor-query").attr("action") ,
-//				type : "POST",
-//				data : {
-//					form :   JSON.stringify( form2js('form-monitor-query', '.', false ) )
-//				},
-//				success : successfullyCreateQuery,
-//				error : manageException,
-//				complete : function() {
-//					btn.classList.remove( "disabled" );
-//					$("#menu").find("#btn-stop-monitor").removeClass("disabled");
-//				}
-//			});
-//		}
-//	});
-//	
-//	$("#btn-stop-monitor").click(function() {
-//		var btn = this;
-//		var btnClassList = btn.classList;
-//		if (!btnClassList.contains("disabled")) {
-//			btnClassList.add("disabled");
-//			stopInterval();
-//			$('#modal-query').modal('show');
-//			$("#btn-add-new-monitor").removeClass("hidden");
-//			$("#btn-stop-monitor").addClass("hidden");
-//		}
-//	});
-	
 });
-//
-//function update() {
-//	var token = $("#token").val();
-//	$.ajax({
-//		url  : $("#form-monitor-query").attr("action"),
-//		type : "POST",
-//		data : {
-//			form : JSON.stringify( 
-//				{ 
-//				 action : "update",
-//				 token : token
-//				}
-//			)
-//		},
-//		success :  updateResultArea,
-//		error :  function(jqXHR, status, errorThrown) {
-//			manageException ( jqXHR , status , errorThrown );
-//			stopInterval();
-//		}
-//	});
-//}
-//
-//function updateResultArea(json){
-//	$("#result").empty();
-//	if (json != "") {
-//		if (json.success && json.data) {
-//			var rows = json.data.rows;
-//			$("#dataAtualizacao").text(json.dataAtualizacao);
-//			if (rows && rows.length > 0) {
-//				var j = rows[0];
-//				var arr = [];
-//				arr.push('<table class="table table-striped"><tr>');
-//				for (k in j) {
-//					arr.push("<th>");
-//					arr.push("" + k);
-//					arr.push("</th>");
-//				}
-//				arr.push("</tr>");
-//				var rowsLength = rows.length;
-//				for (var i = 0; i < rowsLength ; i++) {
-//					var row = rows[i];
-//					arr.push("<tr>");
-//					for (k in row) {
-//						arr.push("<td>");
-//						arr.push(row[k]);
-//						arr.push("</td>");
-//					}
-//					arr.push("</tr>");
-//				}
-//				arr.push('</table>');
-//				$("#result").append( arr.join("") );
-//			}
-//		} else {
-//			$("#result").append("<span style='color:red;'>" + json.message + "</span>");
-//			stopInterval();
-//		}
-//	}
-//}
-//
-//function stopInterval(){
-//	if (interval != null) {
-//		clearInterval(interval);
-//		interval = null;
-//	}
-//}
-//
-///**
-// * Disable all inputs in form-new-monitor
-// */
-//function disableFormNewMonitor (){
-//	$("#form-new-monitor").find("input, select").each(function( index , elem){
-//		$(elem).attr("disabled","disabled");
-//	});
-//};
-//
-//function enableFormNewMonitor(){
-//	$("#form-new-monitor").find("input, select").each(function( index , elem){
-//		$(elem).removeAttr("disabled");
-//	});
-//}
-//
-//function manageException (jqXHR, status, errorThrown) {
-//	if (console) {
-//		console.log("Well that was unexpected" , errorThrown);
-//		console.log("Status: ", status );
-//		console.log("Error: " , jqXHR );
-//	}
-//}
-//
-//function successfullyOpenConnection( data , status, jqXHR) {
-//	var json =  data;
-//	if (json.success) {
-//		$('#modal-monitor').modal('hide');	
-//		$('#modal-query').modal('show');
-//		addSuccessMessage( $("#form-monitor-query-messages")[0] , json.message );
-//		if(json.data)
-//			$("#form-monitor-query").find("#token").val( json.data.token );
-//	} else {
-//		addErrorMessage( $("#form-new-monitor-messages")[0] , json.message );
-//	}
-//}
-//
-//function successfullyCreateQuery( data , status , jqXHR ){
-//	if (data.success) {
-//		updateResultArea(data);
-//		var refreshTime = $("#form-monitor-query").find("#refresh-time").val() || 5;
-//		var monitorTitle = $("#form-monitor-query").find("#monitor-title").val() || "No title defined";
-//		$("#title-monitoring").html(monitorTitle);
-//		interval = window.setInterval( update , refreshTime * 1000);
-//		$('#modal-query').modal('hide');
-//		$("#btn-stop-monitor").removeClass("hidden");
-//		$("#btn-add-new-monitor").addClass("hidden");
-//	}else{
-//		addErrorMessage( $("#form-monitor-query-messages")[0] , data.message );
-//	}
-//}
-//
-//function addSuccessMessage( messageContainer , message ){
-//	clearAlertClass(messageContainer);
-//	var classList = messageContainer.classList;
-//	messageContainer.innerHTML = message;
-//	classList.add("alert-success");
-//	classList.remove("hidden");
-//}
-//
-//function addErrorMessage( messageContainer , message ){
-//	clearAlertClass(messageContainer);
-//	var classList = messageContainer.classList;
-//	messageContainer.innerHTML = message;
-//	classList.add("alert-danger");
-//	classList.remove("hidden");
-//}
-//
-///**
-// * Gets the list of the class of the @container and removes the alert related classes from it, if having any
-// * @param container
-// */
-//function clearAlertClass( container ){
-//	var classList = container.classList;
-//	classList.remove("alert-danger");
-//	classList.remove("alert-info");
-//	classList.remove("alert-warning");
-//	classList.remove("alert-success");
-//}
-//
-/**
- * Verify the sizeof the screen and update the result area to match it
- */
-function resizeTheResultArea() {
-	var titleHeight = $("#row-title").height();
-	var marginBottom = 10;
-	var availableHeight = $(document).height();
-	$("#row-query").height(availableHeight - (titleHeight + marginBottom));
-}
-//
-//function validateNewMonitorForm() {
-//	var divErrors = $("#form-new-monitor-messages").addClass("hidden").empty()[0];
-//	var errors = [];
-//	var host = $("#host").val().replace(/\s/g , "");
-//	if(host.length == 0)
-//		errors.push("<b>Host</b> can't be empty");
-//	var user = $("#user").val();
-//	if(user.length == 0)
-//		errors.push("<b>User</b> can't be empty");
-//	if(errors.length > 0){
-//		addErrorMessage( divErrors, errors.join("<br/>") );
-//		return false;
-//	}
-//	return true;
-//}
-//
-//function validateMonitorQueryForm() {
-//	var divErrors = $("#form-monitor-query-messages").addClass("hidden").empty()[0];
-//	var errors = [];
-//	var query = $("#form-monitor-query").find("#query").val();
-//	if(query.length == 0){
-//		errors.push("<b>Query</b> can't be empty");
-//	}
-//	if(errors.length > 0){
-//		addErrorMessage( divErrors, errors.join("<br/>") );
-//		return false;
-//	}
-//	return true;
-//}
